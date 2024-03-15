d61ef2accbc491d8c784e95686c261e37b5aa834
sabina <sabinaalizada2802@gmail.com>
2024-03-15 21:17:36 +0400

    guards

---
+++ src/main/java/org/example/githubtask/GithubTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  GithubTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubtask/GithubTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
